88004ca06afa95c9a4514e143ef1231cf4aecbea
Dani Camous <Dani.Camous@capitalone.com>
2016-08-11 12:46:48 -0400

    Initial Commit

---
+++ Simple Tipster/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Simple Tipster
+//
+//  Created by Camous, Dani on 8/11/16.
+//  Copyright © 2016 Dani Camous. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
